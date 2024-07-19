128024fbb82719590fcda80de6148f960e7460e9
Mariia <84154912+MKondakk@users.noreply.github.com>
2024-07-19 23:39:31 +0200

    Feature/add popup menu (#10)
    * Added sidebar. Updated global styles
    
    * Added pnpm
    
    * Sorted styles, updated components
    
    * Updated global styles
    
    * Moved page to app, icons to components
    
    * Added tooltip
    
    * Added side popup menu
    
    * Updated imports, changed pages to fc

--- src/components/icons/ExportIcon.tsx
+++ src/components/icons/ExportIcon.tsx
@@ -1,8 +1,9 @@
 import { SVGProps } from "react";
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={12} height={12} {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" width={12} height={12} fill="currentColor" {...props}>
         <path d="M11 10H1a1 1 0 0 0 0 2h10a1 1 0 0 0 0-2Z" />
         <path
+            fill="currentColor"
             d="M6.5 0h-1a.5.5 0 0 0-.5.5V3H2.5a.5.5 0 0 0-.376.829l3.5 4a.5.5 0 0 0 .752 0l3.5-4A.5.5 0 0 0 9.5 3H7V.5a.5.5 0 0 0-.5-.5Z"
             data-color="color-2"
         />
