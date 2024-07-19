edf79e7e747aa317d9e2c7bf636ed6a4c21b6b75
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-07-19 19:42:50 +0300

    Move constant variables outside the component, use callback as arrow function

--- src/app/stage-icon-menu/page.tsx
+++ src/app/stage-icon-menu/page.tsx
@@ -21,39 +21,39 @@ import CopiedBlankIcon from "../../components/icons/CopiedBlankIcon";
 import TwoPagesIcon from "../../components/icons/TwoPagesIcon";
 import StarIcon from "../../components/icons/StarIcon";
 
+const icons: React.ComponentType[] = [
+    DashedCircleIcon,
+    СircleIcon,
+    CircleQuaterIcon,
+    CircleHalfIcon,
+    СircleWithoutQueaterIcon,
+    CheckedCircleIcon,
+    CircleWithStarIcon,
+    BlankIcon,
+    FaceIcon,
+    PersonIcon,
+    LaptopIcon,
+    ShopBagIcon,
+    TwoPagesIcon,
+    CalendarIcon,
+    HeartIcon,
+    ClockIcon,
+    CopiedBlankIcon,
+    StarIcon
+];
+
+const colors: string[] = [
+    "rgba(255, 255, 255, 0.5)",
+    "rgb(153, 92, 191)",
+    "rgb(188, 87, 73)",
+    "rgb(255, 181, 70)",
+    "rgb(92, 106, 228)",
+    "rgb(1, 114, 100)"
+];
+
 export default function StageIconMenu() {
     const [color, setColor] = useState<string>("rgba(255, 255, 255, 0.5)");
     const [activeIndex, setActiveIndex] = useState<number>(0);
-
-    const colors: string[] = [
-        "rgba(255, 255, 255, 0.5)",
-        "rgb(153, 92, 191)",
-        "rgb(188, 87, 73)",
-        "rgb(255, 181, 70)",
-        "rgb(92, 106, 228)",
-        "rgb(1, 114, 100)"
-    ];
-
-    const icons: React.ComponentType[] = [
-        DashedCircleIcon,
-        СircleIcon,
-        CircleQuaterIcon,
-        CircleHalfIcon,
-        СircleWithoutQueaterIcon,
-        CheckedCircleIcon,
-        CircleWithStarIcon,
-        BlankIcon,
-        FaceIcon,
-        PersonIcon,
-        LaptopIcon,
-        ShopBagIcon,
-        TwoPagesIcon,
-        CalendarIcon,
-        HeartIcon,
-        ClockIcon,
-        CopiedBlankIcon,
-        StarIcon
-    ];
 
     function handleColorClick(colorItem: string, index: number) {
         setColor(colorItem);
@@ -63,17 +63,15 @@ export default function StageIconMenu() {
     return (
         <div className={styles.div}>
             <div className={styles.selectColor}>
-                {colors.map(function(colorItem, index) {
+                {colors.map(function (colorItem, index) {
                     return (
                         <div
                             key={index}
                             className={styles.border}
                             style={{
-                                border: activeIndex === index ? `2px solid ${colorItem}` : "none",
+                                border: activeIndex === index ? `2px solid ${colorItem}` : "none"
                             }}
-                            onClick={function() {
-                                handleColorClick(colorItem, index);
-                            }}
+                            onClick={() => handleColorClick(colorItem, index)}
                         >
                             <div
                                 className={styles.circle}
@@ -86,7 +84,7 @@ export default function StageIconMenu() {
                 })}
             </div>
             <ul className={styles.list} style={{ color }}>
-                {icons.map(function(IconComponent, index) {
+                {icons.map(function (IconComponent, index) {
                     return (
                         <li className={styles.item} key={index}>
                             <IconComponent />
