db2a066c1e39c8d086f85b99efc8c109ff546d05
Mariia <mariia.kondak@nure.ua>
2024-07-23 15:51:34 +0200

    resove conflict

---
+++ src/components/icons/AutoModeIcon.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from "react";
+const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 12 12">
+        <title>edit color</title>
+        <g fill="currentColor">
+            <path
+                d="M10.243,1.757a6,6,0,1,0,0,8.486A6,6,0,0,0,10.243,1.757ZM3.172,3.172a4,4,0,0,1,5.656,0L3.172,8.828A4,4,0,0,1,3.172,3.172Z"
+                fill="currentColor"
+            ></path>
+        </g>
+    </svg>
+);
+export default SvgComponent;
