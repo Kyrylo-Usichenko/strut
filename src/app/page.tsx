fbab26c1c17149ecea62e1b3df467897196242a9
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-07-24 19:13:48 +0300

    add trnsition for shadow, change divs to buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ import AccountMenu from "./account-menu/page";
 import s from "./styles.module.css";
 import Sidebar from "~/components/shared/sidebar/Sidebar";
 import GridView from "./grid-view/page";
-import LabelMenu from "./label-menu/page"
+import LabelMenu from "./label-menu/page";
+import LayoutToggler from "./layout-toggler/page";
 
 export default function Home() {
     return (
@@ -40,6 +41,11 @@ export default function Home() {
             <div className={s.item}>
                 <span className={s.title}>Label Menu</span>
                 <LabelMenu />
+                <br /><br /><br /><br /><br />
+            </div>
+            <div className={s.item}>
+                <span className={s.title}>Layout Toggler</span>
+                <LayoutToggler />
             </div>
             <Link href="/stage-icon-menu">stage-icon-menu</Link>
             <Link href="/sidebar">Sidebar</Link>
