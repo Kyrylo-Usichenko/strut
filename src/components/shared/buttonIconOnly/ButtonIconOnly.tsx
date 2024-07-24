170c8714a39c3409f1a2be42b007fd9a6fe05ecf
kekwkekw <danulo2403@gmail.com>
2024-07-24 14:32:57 +0300

    more buttons added

--- src/components/shared/buttonIconOnly/ButtonIconOnly.tsx
+++ src/components/shared/buttonIconOnly/ButtonIconOnly.tsx
@@ -4,36 +4,17 @@ import { Tooltip } from "~/components/shared/Tooltip/Tooltip";
 
 type ButtonProps = {
     icon?: ReactNode;
-    text: string;
     tooltipLabel?: string;
-    withoutBackground?: boolean;
     onClick: () => void;
 };
 
-// export default function Button({ icon, text, tooltipLabel, onClick }: ButtonProps) {
-//     const buttonContent = (
-//         <button className={styles.button} onClick={onClick}>
-//             {icon && <>{icon}</>}
-//             <p className={styles.text}>{text}</p>
-//         </button>
-//     );
-
-//     // return tooltipLabel ? (
-//     //     <Tooltip label={tooltipLabel} direction="bottom">
-//     //         {buttonContent}
-//     //     </Tooltip>
-//     // ) : (
-//     //     buttonContent
-//     // );
-// }
-
-export default function Button({ icon, text, tooltipLabel, withoutBackground, onClick }: ButtonProps) {
+export default function ButtonIconOnly({ icon, tooltipLabel, onClick }: ButtonProps) {
     const [show, setShow] = useState<boolean>(false);
 
     return (
         <>
             <button
-                className={withoutBackground? styles.buttonNoBackground: styles.button}
+                className={styles.button}
                 onClick={onClick}
                 onPointerEnter={() => {
                     if (tooltipLabel) {
@@ -45,7 +26,6 @@ export default function Button({ icon, text, tooltipLabel, withoutBackground, on
                 }}
             >
                 {icon && <>{icon}</>}
-                <p className={styles.text}>{text}</p>
                 {tooltipLabel && show && <Tooltip label={tooltipLabel} direction="bottom" />}
             </button>
         </>
