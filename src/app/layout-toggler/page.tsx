0294958f42413eb406fd664a653648763fd96c6b
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-07-27 18:34:36 +0300

    add new changes to layoutToggler, add ready-made tooltip, remove hover from text when button is active

--- src/app/layout-toggler/page.tsx
+++ src/app/layout-toggler/page.tsx
@@ -5,39 +5,37 @@ import styles from "./styles.module.css";
 import ListViewIcon from "~/components/icons/ListViewIcon";
 import GridViewIcon from "~/components/icons/GridViewIcon";
 import KanbanViewIcon from "~/components/icons/KanbanViewIcon";
+import { Tooltip } from "~/components/shared/Tooltip/Tooltip";
 
 export default function LayoutToggler() {
-  const [active, setActive] = useState<string>('container1');
+    const [active, setActive] = useState<string>("container1");
 
-  return (
-    <div className={styles.div}>
-      <button
-        className={styles.container1}
-        onClick={() => setActive('container1')}
-      >
-        <ListViewIcon />
-        <p className={styles.title1}>List View</p>
-      </button>
-      <button
-        className={styles.container2}
-        onClick={() => setActive('container2')}
-      >
-        <GridViewIcon />
-        <p className={styles.title2}>Grid View</p>
-      </button>
-      <button
-        className={styles.container3}
-        onClick={() => setActive('container3')}
-      >
-        <KanbanViewIcon />
-        <p className={styles.title3}>Kanban View</p>
-      </button>
+    return (
+        <div className={styles.div}>
+            <Tooltip label="List View" direction="bottom">
+                <button className={`${active === "container1" ? styles.container : styles.container1}`} onClick={() => setActive("container1")}>
+                    <ListViewIcon />
+                    {/* <p className={styles.title1}>List View</p> */}
+                </button>
+            </Tooltip>
+            <Tooltip label="Grid View" direction="bottom">
+                <button className={`${active === "container2" ? styles.container : styles.container2}`} onClick={() => setActive("container2")}>
+                    <GridViewIcon />
+                    {/* <p className={styles.title2}></p> */}
+                </button>
+            </Tooltip>
+            <Tooltip label="Kanban View" direction="bottom">
+                <button className={`${active === "container3" ? styles.container : styles.container3}`} onClick={() => setActive("container3")}>
+                    <KanbanViewIcon />
+                    {/* <p className={styles.title3}>Kanban View</p> */}
+                </button>
+            </Tooltip>
 
-      <button
-        className={`${styles.shadow} ${
-          active === 'container2' ? styles.midle : ''
-        } ${active === 'container3' ? styles.right : ''}`}
-      ></button>
-    </div>
-  );
+            <button
+                className={`${styles.shadow} ${
+                    active === "container2" ? styles.midle : ""
+                } ${active === "container3" ? styles.right : ""}`}
+            ></button>
+        </div>
+    );
 }
