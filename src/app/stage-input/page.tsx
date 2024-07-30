92a460c0321be1f7d333d6a3aff816e08d3225e7
kekwkekw <danulo2403@gmail.com>
2024-07-30 20:01:49 +0300

    deployment fix lmao

--- src/app/stage-input/page.tsx
+++ src/app/stage-input/page.tsx
@@ -8,7 +8,7 @@ import DashedCircleIcon from "~/components/icons/DashedCircleIcon";
 import styles from "./styles.module.css";
 import { useVisible } from "~/components/shared/PopupMenu/utils/useVisible";
 
-type stageInputProps = {
+type StageInputProps = {
     viewMode: "list" | "grid" | "kanban";
     icon?: JSX.Element;
     color?: string;
@@ -17,7 +17,7 @@ type stageInputProps = {
     amount?: number;
 };
 
-export default function StageInput({ viewMode, icon, color, value, width, amount}: stageInputProps) {
+export default function StageInput({ viewMode, icon, color, value, width, amount}: StageInputProps) {
     const [currentIcon, setCurrentIcon] = useState(icon || DashedCircleIcon as unknown as JSX.Element);
     const [currentColor, setCurrentColor] = useState(color || "rgba(255, 255, 255, 0.5)");
     const {ref: menuRef, isVisible: showMenu, setIsVisible: setShowMenu} = useVisible(false);
