b8560a60a2a0c2b36140980cc8b84cbcf3d398f1
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-07-31 15:47:02 +0300

    add changes to grid view, fix tooltip, add usage for btns

--- src/app/grid-view/page.tsx
+++ src/app/grid-view/page.tsx
@@ -2,8 +2,8 @@ import styles from "./styles.module.css";
 import PlusIcon from "~/components/icons/PlusIcon"
 import WorkSpaceGridVIew from "~/shared/components/WorkSpaceGridVIew/WorkSpaceGridVIew";
 
-
 export default function GridView() {
+
   return (
     <div className={styles.container}>
       <div className={styles.createContainer}>
--- src/shared/components/WorkSpaceGridVIew/WorkSpaceGridVIew.tsx
+++ src/shared/components/WorkSpaceGridVIew/WorkSpaceGridVIew.tsx
@@ -2,24 +2,37 @@ import styles from "./WorkSpaceGridVIew.module.css";
 import WorkSpaceGridViewIcon from "~/components/icons/WorkSpaceGridViewIcon";
 import ThreeDotsIcon from "~/components/icons/ThreeDotsIcon";
 import { Tooltip } from "~/components/shared/Tooltip/Tooltip";
+import { PopupMenuWithButton } from "~/components/shared/PopupMenu/PopupMenuWithButton";
+import menu from "~/components/shared/PopupMenu/menu.module.css";
+import { items } from "~/components/shared/PopupMenu/PopupMenuWithButton";
+import { useVisible } from "~/components/shared/PopupMenu/utils/useVisible";
+import ButtonIconOnly from "~/components/shared/buttonIconOnly/ButtonIconOnly";
+import { PopupMenu } from "~/components/shared/PopupMenu/PopupMenu";
 
 type Props = {
     title: string;
 };
 export default function WorkSpaceGridVIew({ title }: Props) {
+    const { isVisible, setIsVisible, ref } = useVisible(false);
+    const handleButtonClick = () => {
+        setIsVisible(!isVisible);
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.start}>
                 <WorkSpaceGridViewIcon />
                 <p className={styles.title}>{title}</p>
             </div>
-            <div className={styles.dots}>
-                <Tooltip label="More Options" direction="bottom">
-                    <ThreeDotsIcon />
-                </Tooltip>
-
-                {/* <p className={styles.tooltip}>More Options</p> */}
-            </div>
+            <div className={menu.container} ref={ref}>
+            <ButtonIconOnly
+                onClick={handleButtonClick}
+                icon={<ThreeDotsIcon />}
+                tooltipLabel="More Options"
+                tooltipVisible={!isVisible}
+            />
+            <PopupMenu items={items} direction="bottom" visible={isVisible} />
+        </div>
         </div>
     );
 }
