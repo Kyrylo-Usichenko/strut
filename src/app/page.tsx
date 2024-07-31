477183bf63121a664ab3a7cc9eb05ea6541dfbb5
OleksiyBodnia <90392804+OleksiyBodnia@users.noreply.github.com>
2024-07-31 12:37:09 +0300

    add board list view (#36)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,10 @@ import { PopupMenu } from "~/components/shared/PopupMenu/PopupMenu";
 import { TaskPopup } from "~/components/shared/TaskPopupMenu/TaskPopup";
 import { docInfo, taskItems, TaskPopupWithButton } from "~/components/shared/TaskPopupMenu/TaskPopupWithButton";
 import AccountMenu from "./account-menu/page";
+import BoardListView from "./board-list-view/page";
 import KanbanView from "./kanban-view/page";
 import s from "./styles.module.css";
+
 
 export default function Home() {
     return (
@@ -87,6 +89,11 @@ export default function Home() {
                 <span className={s.title}>Search Input</span>
                 <SearchInput />
             </div>
+            <div className={s.item}>
+                <span className={s.title} style={{width: "700px", paddingLeft: "250px"}}>Board list view</span>
+                <BoardListView />
+            </div>
+            <Link href="/stage-icon-menu">Stage Icon Menu</Link>
             <div className={s.item} >
                 <span className={s.title} style={{paddingLeft: "350px"}}>Kanban View</span>
                 <KanbanView />
---
+++ src/shared/components/BoardListViewItem/BoardListViewItem.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { ReactElement, useState } from "react";
+import styles from "./BoardListViewItem.module.css";
+import ButtonIconOnly from "~/components/shared/buttonIconOnly/ButtonIconOnly";
+import SmallArrowIcon from "~/components/icons/SmallArrowIcon";
+import StageInput from "~/app/stage-input/page";
+import ThreeDotsIcon from "~/components/icons/ThreeDotsIcon";
+import PlusIcon from "~/components/icons/PlusIcon";
+import BoardListViewBottomItem from "../BoardListViewBottomItem/BoardListViewBottomItem";
+
+type Props = {
+    title: string;
+    icon: ReactElement;
+    iconColor: string;
+    number: number;
+    textData: string[];
+};
+
+export default function BoardListViewItem({ title, icon, iconColor, number, textData }: Props) {
+    const [isBottomMenuOpenes, setIsBottomMenuOpenes] = useState<boolean>(false);
+
+    function handleOpenBottomMenu() {
+        setIsBottomMenuOpenes(!isBottomMenuOpenes);
+    }
+
+    function SmallArrow() {
+        return (
+            <div className={`${isBottomMenuOpenes ? styles.smallArrowClicked : styles.smallArrow}`}>
+                <SmallArrowIcon />
+            </div>
+        );
+    }
+
+    function doingNothing() {
+        return;
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.topPart}>
+                <div className={styles.leftPart}>
+                    <div className={styles.ButtonIconOnly}>
+                        <ButtonIconOnly
+                            onClick={handleOpenBottomMenu}
+                            icon={<SmallArrow />}
+                            tooltipLabel={isBottomMenuOpenes ? "Collapse stage" : "Expand Stage"}
+                        />
+                    </div>
+                    <StageInput viewMode="list" color={iconColor} icon={icon} amount={number} value={title} />
+                </div>
+                <div className={styles.rightPart}>
+                    <ButtonIconOnly onClick={doingNothing} icon={<PlusIcon />} tooltipLabel="New Doc" />
+                    <ButtonIconOnly onClick={doingNothing} icon={<ThreeDotsIcon />} tooltipLabel="More Options" />
+                </div>
+            </div>
+
+            {isBottomMenuOpenes && (
+                <ul className={styles.list}>
+                    {textData.map((text, index) => (
+                        <li key={index}>
+                            <BoardListViewBottomItem text={text} icon={icon} iconColor={iconColor}/>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
