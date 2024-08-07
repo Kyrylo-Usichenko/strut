835e9c5e0b5e2938f91a94175e983e0f3b9cf169
Mariia <mariia.kondak@nure.ua>
2024-08-07 16:27:18 +0200

    Fix build error

--- src/app/side-menu-popup/page.tsx
+++ src/app/side-menu-popup/page.tsx
@@ -1,9 +1,8 @@
 import { PopupMenuWithButton } from "~/components/shared/PopupMenu/PopupMenuWithButton";
-import styles from "~/components/shared/PopupMenu/styles.module.css";
 
 function PopupMenuPage() {
     return (
-        <div className={styles.wrapper}>
+        <div>
             <PopupMenuWithButton />
         </div>
     );
