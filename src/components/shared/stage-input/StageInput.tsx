0e6a5b035c6bd8f25ba00f0c772e20282d772e47
Vintonyk Danylo <87479958+win-tonic@users.noreply.github.com>
2024-09-02 21:13:40 +0300

    Features/create-new-stage (#73)
    * +
    
    * added to page
    
    * added parameters to onCreateClick
    
    * example added
    
    * added notification + fixed time sent tooptip positioning + added hover for menu buttons
    
    * removed input transformation on create click
    
    * untitled default value changed to nothing

--- src/components/shared/stage-input/StageInput.tsx
+++ src/components/shared/stage-input/StageInput.tsx
@@ -5,23 +5,37 @@ import IconButton from "./_components/icon-button/IconButton";
 import TextInput from "./_components/text-input/TextInput";
 import DocsAmount from "./_components/docs-amount/DocsAmount";
 import DashedCircleIcon from "~/components/icons/DashedCircleIcon";
+import Button from "../button/Button";
 import styles from "./styles.module.css";
 import { useVisible } from "~/components/shared/PopupMenu/utils/useVisible";
 
 type StageInputProps = {
     viewMode: "list" | "grid" | "kanban";
+    isCreated?: boolean;
     icon?: JSX.Element;
     color?: string;
     value?: string;
     width?: number;
     amount?: number;
+    onCreateClick?: (inputColor: string, inputIcon: JSX.Element, inputText: string) => void;
+    onCancelClick?: () => void;
 };
 
-export default function StageInput({ viewMode, icon, color, value, width, amount }: StageInputProps) {
+export default function StageInput({
+    viewMode,
+    isCreated,
+    icon,
+    color,
+    value,
+    width,
+    amount,
+    onCreateClick,
+    onCancelClick
+}: StageInputProps) {
     const [currentIcon, setCurrentIcon] = useState(icon || (DashedCircleIcon as unknown as JSX.Element));
     const [currentColor, setCurrentColor] = useState(color || "var(--text-color)");
     const { ref: menuRef, isVisible: showMenu, setIsVisible: setShowMenu } = useVisible(false);
-    const [currentValue, setValue] = useState(value || "Untitled");
+    const [currentValue, setValue] = useState(value || "");
     const [currentWidth, setWidth] = useState(width || 67);
 
     function handleIconChange(icon: JSX.Element, color: string) {
@@ -30,7 +44,36 @@ export default function StageInput({ viewMode, icon, color, value, width, amount
         setShowMenu(false);
     }
 
-    return (
+    function createHandler() {
+        onCreateClick?.(currentColor, currentIcon, currentValue);
+    }
+
+    return isCreated ? (
+        <div className={viewMode === "kanban" ? styles.kanbanWrappingDiv : styles.inputDiv}>
+            <div className={styles.inputDiv}>
+                <IconButton
+                    currentIcon={currentIcon}
+                    currentColor={currentColor}
+                    showMenu={showMenu}
+                    onIconChange={handleIconChange}
+                    toggleMenu={() => setShowMenu(!showMenu)}
+                    menuRef={menuRef}
+                />
+                <TextInput
+                    value={currentValue}
+                    width={currentWidth}
+                    styleMode={viewMode}
+                    isCreated={isCreated}
+                    onChange={setValue}
+                    setWidth={setWidth}
+                />
+            </div>
+            <div className={viewMode === "kanban" ? styles.kanbanButtons : styles.buttons}>
+                <Button text={"Create"} onClick={createHandler} />
+                <Button text={"Cancel"} onClick={onCancelClick} />
+            </div>
+        </div>
+    ) : (
         <div className={styles.inputDiv}>
             <IconButton
                 currentIcon={currentIcon}
